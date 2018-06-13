d1df4eba40273735cd4704980560e6d6109b3a67
Yevgeniy Ivanov <anigeka@gmail.com>
2018-06-13 18:11:54 -0500

    4th attempt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
     domain:         'localhost:3000',
-    address:       'gmail.com',
+    address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true
