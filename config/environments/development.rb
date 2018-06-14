4c0b844badd889d180103b0b704ac54ec14fce9d
Yevgeniy Ivanov <anigeka@gmail.com>
2018-06-13 19:34:57 -0500

    9

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'localhost:3000',
     address:       'smtp.gmail.com',
     port:          '587',
