afa8e2a916e18ebae14319ebf0808bc5b371207e
Yevgeniy Ivanov <anigeka@gmail.com>
2018-06-25 16:45:17 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  match '/events', to: 'events#preflight', via: [:options]
+
+  namespace :api, defaults: { format: :json } do
+    resources :events, only: [:create]
+  end
 
   resources :registered_applications, except: :show
   get "read/:id", to: "registered_applications#read"
